6416f34f2ed941546379b8f2b9c6674b4049dafb
mohammadreza.gh <mohammadreza.gharghabi6@gmail.com>
2024-10-06 13:33:55 +0330

    change structure receiver manage_patient_for_newly_reserve for run task as periodic task with clocked schedule

--- online_reservation/signals.py
+++ online_reservation/signals.py
@@ -3,6 +3,8 @@ from django.db.models.signals import post_save
 from django.contrib.auth import get_user_model
 
 from datetime import timezone, timedelta, datetime
+from django_celery_beat.models import ClockedSchedule, PeriodicTask
+import json
 
 from .models import Patient, Doctor, Comment, Reserve
 from .tasks import remove_patient_from_reserve_after_expired
@@ -33,5 +35,14 @@ def remove_doctor_for_change_status_to_rejected(sender, instance, **kwargs):
 @receiver(post_save, sender=Reserve)
 def manage_patient_for_newly_reserve(sender, instance, created, **kwargs):
     if created and instance.reserve_datetime > datetime.now(tz=TEHRAN_TZ):
-        eta = instance.reserve_datetime
-        remove_patient_from_reserve_after_expired.apply_async((instance.id, ), eta=eta)
+        clocked_schedule, created = ClockedSchedule.objects.get_or_create(
+            clocked_time=instance.reserve_datetime
+        )
+
+        task = PeriodicTask.objects.create(
+            name=f'task-for-object-{instance.id}',
+            task='online_reservation.tasks.remove_patient_from_reserve_after_expired',
+            clocked=clocked_schedule,
+            args=json.dumps([instance.id]),
+            one_off=True
+        )
